5fd7231da1457122aaff385561d81394a029316c
pbrazzle <prb50@pitt.edu>
2022-04-30 12:45:57 -0400

    some starting files

---
+++ files.cpp
@@ -0,0 +1,9 @@
+#include <filesystem>
+#include <stdlib.h>
+#include <iostream>
+
+int main()
+{
+	system("msbuild C:/code/personal_project_manager/build/personal_project_manager.sln");
+	return 0;
+}
